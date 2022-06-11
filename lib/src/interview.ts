b784c05dfdede130e59122b1ce3e7033b1ecf5a6
MGWMAN006 <maneno.mgwami@gmail.com>
2022-06-11 13:21:51 +0300

    interview edited as requested and all Tests Passed

--- lib/src/interview.ts
+++ lib/src/interview.ts
@@ -2,7 +2,8 @@ export function getProcessedData(
   yearOneData: any,
   yearTwoData: any,
   yearThreeData: any
-) {
+) 
+{
   // TODO:
   // 1. Remove data items that has reporting rate less than 50
   // 2. Combine relevant data (data of the same code) from each year inorder to compute average
@@ -22,6 +23,72 @@ export function getProcessedData(
       },
     ]
    */
+	
+	let results:any[]=[];
 
-  return;
+	let regCount:number=0;
+	let regSum:number=0;
+    
+	let activeCount:number=0;
+	let activeSum:number=0;
+
+	let regRate1:number=yearOneData[0].reportingRate;
+	if(regRate1 >= 50)
+	{
+		regSum=regSum+yearOneData[0].value;
+		regCount++;
+	}
+    
+	let regRate2:number=yearTwoData[0].reportingRate;
+	if(regRate2 >= 50)
+        {
+                regSum=regSum+yearTwoData[0].value;
+                regCount++;
+        }
+
+	let regRate3:number=yearThreeData[0].reportingRate;
+	if(regRate3 >= 50)
+        {
+                regSum=regSum+yearThreeData[0].value;
+                regCount++;
+        }
+
+
+	let activeRate1:number=yearOneData[1].reportingRate;
+	if(activeRate1 >= 50)
+        {
+                activeSum=activeSum+yearOneData[1].value;
+                activeCount++;
+        }
+
+	let activeRate2:number=yearTwoData[1].reportingRate;
+	if(activeRate2 >= 50)
+        {
+                activeSum=activeSum+yearTwoData[1].value;
+                activeCount++;
+        }
+	let activeRate3:number=yearThreeData[1].reportingRate;
+	if(activeRate3 >= 50)
+        {
+                activeSum=activeSum+yearThreeData[1].value;
+                activeCount++;
+        }
+
+	let avgReg:number=regSum/regCount;
+	let avgActive:number=activeSum/activeCount;
+
+	results.push( {
+        value: avgReg,
+        name: 'Number of members registered',
+        code: 'MEMBER_REGISTERED'
+	}); 
+
+	results.push(
+	{
+        value: avgActive,
+        name: 'Number of active members',
+        code: 'ACTIVE_MEMBERS'
+	});
+
+	return results;
 }
